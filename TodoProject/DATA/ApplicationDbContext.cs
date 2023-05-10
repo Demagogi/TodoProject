fac145f0560881461b2622d2b8bbc61f53201146
Demagogi <mr.didebulidze@gmail.com>
2023-05-10 14:31:47 +0400

    First Problem, save before start solving it

--- TodoProject/DATA/ApplicationDbContext.cs
+++ TodoProject/DATA/ApplicationDbContext.cs
@@ -8,9 +8,18 @@ namespace TodoProject.DATA
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
+
         }
 
         public DbSet<ToDoList> ToDoList { get; set; }
         public DbSet<ToDoListItems> ToDoListItems { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ToDoListItems>().HasData(new ToDoListItems {Id=1 , Title = "Test Task Item", Description = "This is test task item for vizualization" });
+            modelBuilder.Entity<ToDoList>().HasData(
+            new ToDoList { Id = 1, Name = "Test Task", Description = "This is test task for vizualization" }
+            );
+        }
     }
 }
--- TodoProject/Models/ToDoListItems.cs
+++ TodoProject/Models/ToDoListItems.cs
@@ -3,7 +3,6 @@
     public class ToDoListItems
     {
         public int Id { get; set; }
-        public int ToDoListId { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
     }
