e2883d2e066763cbcbdfdb3aed1f3219fe7bce29
Demagogi <mr.didebulidze@gmail.com>
2023-05-10 14:21:41 +0400

    First Flag, everything works ATM!

---
+++ TodoProject/Controllers/ToDoListItemsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TodoProject.Controllers
+{
+    public class ToDoListItemsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
