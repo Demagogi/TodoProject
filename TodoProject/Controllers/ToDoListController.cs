96916660a10d188729993e916a77e7b6a25e59be
Demagogi <mr.didebulidze@gmail.com>
2023-05-10 15:59:49 +0400

    Added Create ActionMethod for ToDoList

--- TodoProject/Controllers/ToDoListController.cs
+++ TodoProject/Controllers/ToDoListController.cs
@@ -1,12 +1,35 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TodoProject.DATA;
+using TodoProject.Models;
 
 namespace TodoProject.Controllers
 {
     public class ToDoListController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public ToDoListController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
+            List<ToDoList> objectToDoList = _db.ToDoList.ToList();
+            return View(objectToDoList);
+        }
+
+        public IActionResult Create()
+        {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(ToDoList obj) 
+        {
+            _db.ToDoList.Add(obj); 
+            _db.SaveChanges(); 
+            return RedirectToAction("Index", "ToDoList"); 
         }
     }
 }
--- TodoProject/Controllers/ToDoListItemsController.cs
+++ TodoProject/Controllers/ToDoListItemsController.cs
@@ -1,12 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TodoProject.DATA;
+using TodoProject.Models;
 
 namespace TodoProject.Controllers
 {
     public class ToDoListItemsController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public ToDoListItemsController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            List<ToDoListItems> objectToDoListItem = _db.ToDoListItems.ToList();
+            return View(objectToDoListItem);
         }
     }
 }
